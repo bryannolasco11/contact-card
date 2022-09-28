99c584d833ee95473de40bd9f9710001d5e157c3
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-27 19:36:26 -0500

    delete card works

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,7 @@ import "./form";
 import { initDb, getDb, postDb } from './database';
 import { fetchCards } from './cards';
 import { toggleForm, clearForm } from "./form";
+import { deleteDb } from "./database";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,50 +13,59 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     initDb();
     fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
-  })
+})
 
-    // Form functionality
+// Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
 let submitBtnToUpdate = false;
 let profileId;
 
 newContactButton.addEventListener('click', event => {
-  toggleForm()
- })
+    toggleForm()
+})
 
 form.addEventListener('submit', event => {
-  // Handle data
-  event.preventDefault();
-let name = document.getElementById("name").value;
-let phone = document.getElementById("phone").value;
-let email = document.getElementById("email").value;
-let profile = document.querySelector('input[type="radio"]:checked').value;
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
 
-  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
-if (submitBtnToUpdate == false) {
-  postDb(name, email, phone, profile);
-} else {
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
 
-  fetchCards();
-    // Toggles the submit button back to POST functionality
-  submitBtnToUpdate = false;
-}
+        fetchCards();
+        // Toggles the submit button back to POST functionality
+        submitBtnToUpdate = false;
+    }
 
-// Clear form
-clearForm();
-// Toggle form
-toggleForm();
-// Reload the DOM
-fetchCards();
+    // Clear form
+    clearForm();
+    // Toggle form
+    toggleForm();
+    // Reload the DOM
+    fetchCards();
 });
 
-  // Import CSS files
+window.deleteCard = (e) => {
+    // Grabs the id from the button element attached to the contact card.
+    let id = parseInt(e.id);
+    // Delete the card
+    deleteDb(id);
+      // Reload the DOM
+    fetchCards();
+  };
+
+// Import CSS files
 import "../css/index.css";
 
