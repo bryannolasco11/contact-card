594163193cbe413c323b28837975ce4da6907f4e
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-28 16:36:39 -0500

    installed pwa

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -28,6 +28,24 @@ const newContactButton = document.getElementById("new-contact");
 let submitBtnToUpdate = false;
 let profileId;
 
+// Install button
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        installBtn.setAttribute('disabled', true);
+        installBtn.textContent = 'Installed!';
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+});
+
 newContactButton.addEventListener('click', event => {
     toggleForm()
 })
